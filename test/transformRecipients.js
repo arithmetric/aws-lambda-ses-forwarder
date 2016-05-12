34606708b6f61f08a140286d502345ba45972b5b
Joe Turgeon <arithmetric@gmail.com>
2016-05-12 08:14:47 -0500

    Adding test coverage and documentation for domain wildcard matching.

--- test/transformRecipients.js
+++ test/transformRecipients.js
@@ -33,13 +33,63 @@ describe('index.js', function() {
         });
       });
 
-    it('should stop the process if there are no new recipients',
+    it('should transform recipients according a domain wildcard mapping',
+      function(done) {
+        var data = {
+          recipients: ["info@example.com"],
+          config: {
+            forwardMapping: {
+              "@example.com": [
+                "jim@example.com",
+                "jane@example.com"
+              ]
+            },
+            log: console.log
+          }
+        };
+        index.transformRecipients(data, function(err, data) {
+          assert.ok(!err, "transformRecipients returned successfully");
+          assert.equal(data.recipients[0],
+            "jim@example.com",
+            "parseEvent made 1/2 substitutions");
+          assert.equal(data.recipients[1],
+            "jane@example.com",
+            "parseEvent made 2/2 substitutions");
+          done();
+        });
+      });
+
+    it('should exit if there are no new recipients',
       function(done) {
         var data = {
           recipients: ["noreply@example.com"],
           config: {
             forwardMapping: {
               "info@example.com": [
+                "jim@example.com"
+              ]
+            }
+          },
+          context: {
+            succeed: function() {
+              done();
+            }
+          },
+          log: console.log
+        };
+        index.transformRecipients(data, function() {
+          assert.ok(false, "transformRecipients should not invoke callback");
+          done();
+        });
+      });
+
+    it('should exit if the recipient is malformed',
+      function(done) {
+        var data = {
+          recipients: ["example.com"],
+          config: {
+            forwardMapping: {
+              "@example.com": [
                 "jim@example.com"
               ]
             }
