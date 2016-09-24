9fbb6fddca500a929fcce30832d8d14b27b66b80
Joe Turgeon <arithmetric@gmail.com>
2016-09-24 17:39:15 -0500

    Adding tests for case insensitive match for the Reply-To header (see 51656944b).

--- test/processMessage.js
+++ test/processMessage.js
@@ -53,6 +53,29 @@ describe('index.js', function() {
       });
     });
 
+    it('should preserve an existing Reply-to header', function(done) {
+      var data = {
+        config: {},
+        email: {
+          source: "betsy@example.com"
+        },
+        emailData:
+          fs.readFileSync("test/assets/message.replyto_case.txt").toString(),
+        log: console.log,
+        recipients: ["jim@example.com"],
+        originalRecipient: "info@example.com"
+      };
+      var emailDataProcessed = fs.readFileSync(
+        "test/assets/message.replyto_case.processed.txt").toString();
+      index.processMessage(data, function(err, data) {
+        assert.ok(!err, "processEmail returned successfully");
+        assert.equal(data.emailData,
+          emailDataProcessed,
+          "processEmail updated email data");
+        done();
+      });
+    });
+
     it('should allow overriding the From header in emails', function(done) {
       var data = {
         config: {
