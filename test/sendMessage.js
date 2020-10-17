e027a3093939881082746e533bc26fbb2fb00905
Thomas T. Jarløv <ThomasTJdev@users.noreply.github.com>
2020-10-17 06:25:48 +0200

    Update sendMessage.js

--- test/sendMessage.js
+++ test/sendMessage.js
@@ -56,5 +56,67 @@ describe('index.js', function() {
             done();
           });
       });
+    
+    it('should invoke the AWS SES SDK to send the bounce message',
+      function(done) {
+        var data = {
+          config: {
+            notifyEmail: "MAILER-DAEMON@example.com",
+            notify550: true,
+            notify552: true,
+          },
+          smtpErr: "552",
+          recipients: [
+            "jim@example.com"
+          ],
+          originalRecipients: [
+            "info@example.com"
+          ],
+          emailData: "message data",
+          context: {},
+          log: console.log,
+          ses: {
+            sendEmail: function(options, callback) {
+              callback(null, {status: "ok"});
+            }
+          }
+        };
+        index.sendMessage(data)
+          .then(function() {
+            assert.ok(true, "sendMessage returned successfully");
+            done();
+          });
+      });
+    
+    it('should result in failure if the AWS SES SDK cannot send the bounce message',
+      function(done) {
+        var data = {
+          config: {
+            notifyEmail: "MAILER-DAEMON@example.com",
+            notify550: true,
+            notify552: true,
+          },
+          smtpErr: "550",
+          recipients: [
+            "jim@example.com"
+          ],
+          originalRecipients: [
+            "info@example.com"
+          ],
+          emailData: "message data",
+          context: {},
+          log: console.log,
+          ses: {
+            sendEmail: function(options, callback) {
+              callback(true);
+            }
+          }
+        };
+        index.sendMessage(data)
+          .catch(function(err) {
+            assert.ok(err, "sendMessage bounce msg aborted operation");
+            done();
+          });
+      });
   });
 });
